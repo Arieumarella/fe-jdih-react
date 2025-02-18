cd08f83ba6b8bc067a185f40661ae5a8342e73a8
Arieumarella <al.razli.umarella.15111020@gmail.com>
2025-02-18 16:29:43 +0700

    fix footer mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,16 @@ const Footer = () => {
     return (
       <footer className="pt-4 text-center bg-slate-100">
 
-      <section className="px-5 md:px-[60px] justify-between flex font-roboto text-bluePu my-8">
+      <section className="px-5 md:px-[60px] justify-between md:flex font-roboto text-bluePu my-8">
 
-      <div className="text-left  w-full">
+      <div className="text-left w-full">
        
-        <div className="flex group text-[18px] w-[350px] gap-2 items-center ">
+        <div className="flex group md:text-[18px] text-[16px] md:w-[350px] gap-2 items-center ">
           <img src={logoPu} alt="logo pu" className="h-[50px] " />
           <p className="font-bold font-onest">Jaringan Dokumentasi <br />dan Informasi Hukum</p>
         </div>
 
-        <div className="text-[18px] w-[500px] gap-2 items-center mt-10 font-onest">
+        <div className="md:text-[18px] text-[14px] md:w-[500px] w-full gap-2 items-center md:mt-10 mt-6 font-onest">
         <p>Sekretariat Jenderal<br/>
         Kementerian Pekerjaan Umum 
         Jl. Pattimura No.20,
@@ -21,7 +21,7 @@ const Footer = () => {
         Kota Jakarta Selatan, DKI Jakarta 12110, Indonesia,</p>
         </div>
 
-        <div className="text-[158x] w-[400px] items-center mt-8 font-onest">
+        <div className="md:text-[158x] text-[14px] md:w-[400px] w-full items-center md:mt-8 mt-4 font-onest">
           <p className="my-1"><i class="fa-solid fa-phone mr-2"></i> (021) 739-6783</p>
           <p className="my-1`"><i class="fa-solid fa-phone mr-2"></i> (021) 723-5216</p>
           <p className="my-1"><i class="fa-solid fa-envelope mr-2"></i> jdih@pu.go.id</p>
@@ -29,21 +29,21 @@ const Footer = () => {
         
       </div>
 
-      <div className=" w-full text-left">
-        <p className="font-bold text-bluePu text-[30px] font-onest">Temukan Kami :</p>
-        <div className="flex mx-1 my-4 text-bluePu gap-5">
-          <a href=""><i class="fa-brands fa-instagram fa-4x" ></i></a>
-          <a href=""><i class="fa-brands fa-twitter fa-4x" ></i></a>
-          <a href=""><i class="fa-brands fa-facebook fa-4x" ></i></a>
-          <a href=""><i class="fa-brands fa-youtube fa-4x" ></i></a>
-          <a href=""><i class="fa-brands fa-google-play fa-4x" ></i></a>
+      <div className=" w-full text-left md:mt-0 mt-6">
+        <p className="font-bold text-bluePu md:text-[30px] text-[18px] font-onest">Temukan Kami :</p>
+        <div className="flex mx-1 md:my-4 my-0 text-bluePu md:gap-5 gap-2">
+          <a href=""><i class="fa-brands fa-instagram md:fa-4x fa-2x" ></i></a>
+          <a href=""><i class="fa-brands fa-twitter md:fa-4x fa-2x" ></i></a>
+          <a href=""><i class="fa-brands fa-facebook md:fa-4x fa-2x" ></i></a>
+          <a href=""><i class="fa-brands fa-youtube md:fa-4x fa-2x" ></i></a>
+          <a href=""><i class="fa-brands fa-google-play md:fa-4x fa-2x" ></i></a>
         </div>
       </div>
 
       <div className="text-left  w-full">
-        <div className="group ml-[300px]">
-          <p className="font-bold text-bluePu text-[23px] font-onest">SITEMAP</p>
-          <ul className="mt-2 space-y-2 text-bluePu text-[16px] font-onest font-semibold group">
+        <div className="group md:ml-[300px] md:mt-0 mt-6">
+          <p className="font-bold text-bluePu md:text-[23px] text-[18px] font-onest">SITEMAP</p>
+          <ul className="mt-2 space-y-2 text-bluePu md:text-[16px] text-[14px] font-onest font-semibold group">
               <li className="flex items-center"><span class="material-symbols-outlined">arrow_right</span>  <a href="/" className="hover:text-slate-900">Beranda</a></li>
               <li className="flex items-center"><span class="material-symbols-outlined">arrow_right</span> <a href="/about" className="hover:text-slate-900">Tentang Kami</a></li>
               <li className="flex items-center"><span class="material-symbols-outlined">arrow_right</span>  <a href="/services" className="hover:text-slate-900">Layanan</a></li>
@@ -56,7 +56,7 @@ const Footer = () => {
       </section>
 
       <section className="bg-bluePu w-full p-1">
-      <div className="text-center text-slate-100 font-onest py-4 font-semibold font-onest text-[14px]">Hak Cipta © 2025 Kementerian Pekerjaan Umum Republik Indonesia. All Rights Reserved</div>
+      <div className="text-center text-slate-100 font-onest py-4 font-semibold font-onest md:text-[14px] text-[10px]">Hak Cipta © 2025 Kementerian Pekerjaan Umum Republik Indonesia. All Rights Reserved</div>
       </section>
 
       </footer>
