56945199b7973a8c7b5a1fe1818234c191ebfbc5
Arieumarella <al.razli.umarella.15111020@gmail.com>
2025-02-26 23:12:00 +0700

    fix home mobail dan desktop

--- src/components/Langganan.jsx
+++ src/components/Langganan.jsx
@@ -15,10 +15,10 @@ const Langganan = () => {
     <section className='bg-bluePu text-white  text-center mt-4  md:flex md:justify-between md:px-[60px] items-center'>
 
     <div className='w-full md:flex md:justify-center'>
-      <p className='md:absolute font-semibold font-roboto md:text-[25px] text-[20px] py-4 text-kuningButton text-center'>Indeks Kepuasan Masyarakat</p>
+      <p className='md:absolute font-semibold font-roboto md:text-[25px] text-[20px] py-4 md:mt-[18px] text-kuningButton text-center'>Indeks Kepuasan Masyarakat</p>
      
-      <div className='md:flex group w-full '>
-        <div className="md:relative flex items-center justify-center w-48 h-48 rounded-full p-[13px] bg-gradient-to-r from-gradiankuning to-gradianBiru md:my-auto md:mx-4 mx-auto group">
+      <div className='md:flex md:mt-[55px] md:mb-[30px] group w-full '>
+        <div className="md:relative flex items-center justify-center w-48 h-48 rounded-full p-[13px] bg-gradient-to-r from-gradiankuning to-gradianBiru md:my-auto md:mx-1 mx-auto group">
           <div className="w-[93%] h-[93%] bg-[#D9D9D9] rounded-full flex items-center justify-center">
             <div className="w-[95%] h-[95%] bg-bluePu rounded-full  items-center justify-center">
               <p className="text-2xl font-bold text-slate-200 font-onest text-[35px] mt-[35px]">4.1</p>
@@ -29,7 +29,7 @@ const Langganan = () => {
         </div>
         
       
-        <div className="relative items-center flex justify-centermy-auto my-5"> 
+        <div className="relative items-center flex md:ml-2 my-4 md:w-[250px] md:pl-6"> 
           
           <div className='md:flex md:flex-col font-roboto gap-1'>        
             <div className='inline-flex items-center space-x-2 md:w-auto w-[70%]'>
@@ -79,23 +79,38 @@ const Langganan = () => {
           </div>
         </div>
 
-        <div className=" md:w-[42%] md:h-48 my-auto group">
-         <p className='text-left mt-3 font-roboto md:text-[16px] flex-col'>Terima Kasih Telah Mengirimkan Indeks Kepuasan Masyarakat</p>
-         <button type='button' className='my-4 bg-kuningButton hover:bg-yellow-300 items-end text-bluePu font-onest text-[15px] font-semibold md:w-[100px] w-[75px] md:h-[40px] h-[30px] rounded-xl mr-[280px] hover:bg-yellow-500 active:bg-yellow-600'>Review</button>
+        <div className=" md:w-[42%] md:h-48 mx-10 md:mt-[30px] group">
+         <p className='md:text-left text-center mt-3 font-roboto md:text-[16px] flex-col'>Terima Kasih Telah Mengirimkan Indeks Kepuasan Masyarakat</p>
+         <button type='button' className='my-4 bg-kuningButton hover:bg-yellow-300 text-bluePu font-onest text-[15px] font-semibold md:w-[100px] w-[75px] md:h-[40px] h-[30px] rounded-xl md:mr-[280px] hover:bg-yellow-500 active:bg-yellow-600'>Review</button>
         </div>
 
       </div>
     </div>
       
-      {/* <div className='group w-full'>
+      <div className='group w-full md:my-0 my-10 md:mb-[40px]'>
           <p className='font-roboto font-semibold md:text-[25px] text-[20px] py-4 text-kuningButton'>Berlangganan</p>
-          <p className='font-roboto px-2 font-medium md:text-[16px] text-[14px]'>Anda dapat berlangganan untuk mendapatkan notifikasi dan info penting di bidang pekerjaan umum dari pengelola JDIH PU langsung lewat inbox email Anda. </p>
-          <div className="flex flex-col items-center relative  w-full group">
-              <input type="text" placeholder=" Masukan Nama.." className="border border-slate-100 shadow-lg bg-slate-100 text-slate-800 rounded-xl md:w-[500px] w-full md:h-[40px] h-[35px] my-4 placeholder:font-onest placeholder:text-slate-500 placeholder:text-[14px]"/>
-              <input type="text" placeholder=" Masukan Email.." className="border border-slate-100 shadow-lg bg-slate-100 rounded-xl md:w-[500px] w-full md:h-[40px] h-[35px] my-1 placeholder:font-onest placeholder:text-slate-500 placeholder:text-[14px]" />
-              <button type='button' className='ml-[400px] my-4 bg-kuningButton hover:bg-yellow-300 items-end text-bluePu font-onest text-[15px] font-semibold md:w-[100px] w-[75px] md:h-[40px] h-[30px] rounded-xl relative hover:bg-yellow-500 active:bg-yellow-600'>Kirim</button>
+          <p className='font-roboto md:px-2 px-10 font-medium md:text-[16px] text-[14px]'>Anda dapat berlangganan untuk mendapatkan notifikasi dan info penting di bidang pekerjaan umum dari pengelola JDIH PU langsung lewat inbox email Anda. </p>
+          <div className="flex flex-col items-center relative w-full group">
+              <input type="text" placeholder=" Masukan Nama.." className="border border-slate-100 shadow-lg bg-slate-100 text-slate-800 rounded-xl md:w-[500px] w-[290px] md:h-[40px] h-[35px] my-4 placeholder:font-onest placeholder:text-slate-500 placeholder:text-[14px]"/>
+             
+              <div className="relative md:w-[500px] w-[290px]">
+  
+              <input 
+                type="text" 
+                placeholder="Masukan Email.." 
+                className="border border-slate-100 shadow-lg bg-slate-100 rounded-xl md:w-[500px] w-[290px] md:h-[40px] h-[35px] my-1 placeholder:font-onest placeholder:text-slate-500 placeholder:text-[14px] pr-12"
+              />
+              <button 
+                type="submit" 
+                className="md:block hidden absolute right-2 top-1/2 -translate-y-1/2 bg-bluePu text-kuningButton font-roboto w-[100px] px-3 py-1 rounded-xl "
+              >
+                Kirim
+              </button>
+            </div>
+  
+              <button type='button' className='md:hidden block md:ml-[400px] my-4 bg-kuningButton hover:bg-yellow-300 items-end text-bluePu font-onest text-[15px] font-semibold md:w-[100px] w-[75px] md:h-[40px] h-[30px] rounded-xl relative hover:bg-yellow-500 active:bg-yellow-600'>Kirim</button>
           </div>
-        </div> */}
+        </div>
     
     </section>
     </>
