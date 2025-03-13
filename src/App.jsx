c3dbfcc6b24f20e410d2f27a6ce282f91b7927fa
Arieumarella <al.razli.umarella.15111020@gmail.com>
2025-03-13 14:36:42 +0700

    fix dynemic tittle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Berita from "./pages/Berita";
 import Monografi from "./pages/Monografi";
@@ -17,28 +16,73 @@ import DetailArtikel from "./pages/DetailArtikel";
 import TentangKami from "./pages/TentangKami";
 import Prasyarat from "./pages/Prasyarat";
 import KontakKami from "./pages/KontakKami";
+import { Helmet } from "react-helmet-async";
+
+
+
+const titles = {
+  "/": "JDIH PU - Home",
+  "/Search/pencarian-detail": "JDIH PU - Pencarian Deail",
+  "/Search/pencarian-biasa": "JDIH PU - Pencarian",
+  "/detail-dokumen/:slug": "JDIH PU - Detail Dokumen",
+  "/Berita": "JDIH PU - List Berita",
+  "/Berita/:slug": "JDIH PU - Detail Berita",
+  "/Monografi": "JDIH PU - List Monografi",
+  "/Monografi/:slug": "JDIH PU - Detail Monografi",
+  "/putasn-pengadilan": "JDIH PU - List Putusan Pengadilan",
+  "/putasn-pengadilan/:slug": "JDIH PU - Detail Putusan Pengadilan",
+  "/agenda": "JDIH PU - List Agenda",
+  "/artikel": "JDIH PU - List Artikel",
+  "/artikel/:slug": "JDIH PU - Detail Artikel",
+  "/struktur-organisasi": "JDIH PU - Struktur Organisasi",
+  "/tentang-kami": "JDIH PU - Tentang Kami",
+  "/prasyarat": "JDIH PU - Prsayarat",
+  "/kontak-kami": "JDIH PU - Kontak Kami",
+};
+
+function TitleUpdater() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  // Cek apakah ada title yang cocok tanpa parameter dinamis
+  const matchedTitleKey = Object.keys(titles).find((route) =>
+    new RegExp(`^${route.replace(/:\w+/g, ".*")}$`).test(currentPath)
+  );
+
+  const title = titles[matchedTitleKey] || "JDIH PU";
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+    </Helmet>
+  );
+}
+
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Berita" element={<Berita />} />
-      <Route path="/Monografi" element={<Monografi />} />
-      <Route path="/Search/:tipePencarian" element={<Search />} />
-      <Route path="/detail-dokumen/:slug" element={<DetailDokumen />} />
-      <Route path="/Berita/:slug" element={<DetailBerita />} />
-      <Route path="/Monografi/:slug" element={<Detailmonografi />} />
-      <Route path="/struktur-organisasi" element={<StrukturOrganisasi />} />
-      <Route path="/putasn-pengadilan" element={<PutusanPengadilan />} />
-      <Route path="/putasn-pengadilan/:slug" element={<DetailPutusan />} />
-      <Route path="/agenda" element={<Agenda />} />
-      <Route path="/artikel" element={<Artikel />} />
-      <Route path="/artikel/:slug" element={<DetailArtikel />} />
-      <Route path="/tentang-kami" element={<TentangKami />} />
-      <Route path="/prasyarat" element={<Prasyarat />} />
-      <Route path="/kontak-kami" element={<KontakKami />} />
-    </Routes>
+    <>
+      <TitleUpdater />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Berita" element={<Berita />} />
+        <Route path="/Monografi" element={<Monografi />} />
+        <Route path="/Search/:tipePencarian" element={<Search />} />
+        <Route path="/detail-dokumen/:slug" element={<DetailDokumen />} />
+        <Route path="/Berita/:slug" element={<DetailBerita />} />
+        <Route path="/Monografi/:slug" element={<Detailmonografi />} />
+        <Route path="/struktur-organisasi" element={<StrukturOrganisasi />} />
+        <Route path="/putasn-pengadilan" element={<PutusanPengadilan />} />
+        <Route path="/putasn-pengadilan/:slug" element={<DetailPutusan />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/artikel" element={<Artikel />} />
+        <Route path="/artikel/:slug" element={<DetailArtikel />} />
+        <Route path="/tentang-kami" element={<TentangKami />} />
+        <Route path="/prasyarat" element={<Prasyarat />} />
+        <Route path="/kontak-kami" element={<KontakKami />} />
+      </Routes>
+    </>
   )
 }
 
